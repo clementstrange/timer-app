9f9fce0177ebdddd76ac337fdca004f32808d226
clement.strange <35303942+clementstrange@users.noreply.github.com>
2025-07-14 11:00:13 +0400

    Finish button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -704,7 +704,7 @@ function App() {
       "Resume"}
 </button>
             
-            {sessionType === "work" ? (
+            {sessionType === "work" && (timerState === "running" || timerState === "paused") ? (
               <button 
                 style={secondaryButtonStyle}
                 onClick={reset}
